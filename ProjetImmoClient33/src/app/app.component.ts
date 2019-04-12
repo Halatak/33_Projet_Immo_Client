c22b1ff1600632ea6da261b642ef823840fe5375
tarzan <antonin.bourdieu@gmail.com>
2019-04-12 12:34:53 +0200

    Antonin : carousel

--- ProjetImmoClient33/src/app/app.component.ts
+++ ProjetImmoClient33/src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+
 })
 export class AppComponent {
   title = 'ProjetImmoClient33';
+  customOptions: any = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      2160: {
+        items: 2
+      },
+      4320: {
+        items: 3
+      },
+      8640: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 }
+
--- ProjetImmoClient33/src/app/app.module.ts
+++ ProjetImmoClient33/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +20,11 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+     RouterModule.forRoot([])
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
