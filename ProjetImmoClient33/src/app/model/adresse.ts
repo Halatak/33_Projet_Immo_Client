6fd90074d05205d194c1cc20402c3c51c6b66a0d
Halatak <cangi@laposte.net>
2019-04-15 12:05:59 +0200

    arthru : adressets

--- ProjetImmoClient33/src/app/model/adresse.ts
+++ ProjetImmoClient33/src/app/model/adresse.ts
@@ -1,47 +1,18 @@
 export class Adresse {
 
   // Attributs de la classe
-  private _rue: string;
-  private _numero: number;
-  private _codePostal: number;
-  private _ville: string;
+  public rue: string;
+  public numero: number;
+  public codePostal: number;
+  public ville: string;
 
   // constructeurs
   constructor() {
   }
 
-  // getter et setter
-  get rue(): string {
-    return this._rue;
-  }
-  set rue(newRue: string) {
-    this._rue = newRue;
-  }
-
-  get numero(): number {
-    return this._numero;
-  }
-  set numero(newNumero: number) {
-    this._numero = newNumero;
-  }
-
-  get codePostal(): number {
-    return this._codePostal;
-  }
-  set codePostal(newCodePostal: number) {
-    this._codePostal = newCodePostal;
-  }
-
-  get ville(): string {
-    return this._ville;
-  }
-  set ville(newVille: string) {
-    this._ville = newVille;
-  }
-
   // ToString
   public toString = (): string => {
-    return `${this._numero} ${this._rue} ${this._codePostal} ${this._ville}`;
+    return `${this.numero} ${this.rue} ${this.codePostal} ${this.ville}`;
   }
 
 }
